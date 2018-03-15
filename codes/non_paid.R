55349ab8f0e022d8ce573f6337fb1ff4a76885c4
Harry Yang <harry.seunghoon.yang@gmail.com>
2018-03-15 16:07:46 +0900

    update 2

--- codes/non_paid.R
+++ codes/non_paid.R
@@ -29,3 +29,25 @@ non_paid %>%
   scale_x_date(date_breaks = "1 year") +
   ggthemes::theme_economist_white()
 
+
+price_rate <- read_excel("datasets/monthly_rate.xlsx")
+
+month_price <- seq(as.Date("2010-01-01"), as.Date("2018-01-01"), "months")
+
+price_rate$`지 역` <- c("경남전체", "거제")
+price_rate$X__1 <- NULL
+
+price_rate <- price_rate %>% 
+  gather(month, rate, `2010년 01월`:`2018년 01월`)
+
+price_rate <- price_rate %>% 
+  mutate(month = rep(month_price, each = 2))
+str(price_rate)
+
+price_rate %>% 
+  filter(month >= "2013-01-01") %>% 
+  ggplot() + 
+  geom_line(aes(month, rate, group = `지 역`, color = `지 역`)) +
+  scale_x_date(date_breaks = "1 year") +
+  ggthemes::theme_economist() +
+  labs(title = "지가지수 변동", subtitle = "경남전체와 거제 비교(2013~2018)", ylab = "지수", xlab = "월")
